b398c5d55bdf1b089010d76a4bff686fce468acd
sreyas <sreyassreelal@gmail.com>
2017-04-14 15:29:58 +0530

    Plugin Installer implemented
    Now users can use this to download samp plugins. Just ask samphelper
    nicely.

--- samphelper.py
+++ samphelper.py
@@ -6,14 +6,50 @@ from colorama import Fore, Back, Style
 
 check = True;
 wikiurl = "http://wiki.sa-mp.com/wiki/";
+githuburl="https://www.github.com";
 function_name = "";
 function_description = "";
 function_parameters = "";
 NeedMore = "";
-print( Style.NORMAL );
-print( "\n\n\t\t\t\t" + Fore.WHITE + "SAMP HELPER" + Fore.MAGENTA + " PYTHON TOOL " + Fore.GREEN + "BY" + Fore.RED + " SREYAS" );
+plugin_name = "";
+downloaded_plugin = False;
+option = "";
 
-while check == True:
+def download_file( url ):
+    local_filename = url.split( '/' )[ -1 ]
+    
+    r = requests.get( url , stream = True )
+    with open( local_filename , 'wb' ) as f:
+        for chunk in r.iter_content( chunk_size = 1024 ): 
+            if chunk: 
+                f.write( chunk )
+                
+    return local_filename
+
+def GetPlugin( ):
+	print( Fore.WHITE + "Input plugin name to download " );
+	plugin_name = input( );
+
+	req = requests.get( "https://github.com/search?l=C%2B%2B&q=topic%3Asa-mp+" + plugin_name + "&type=Repositories" );
+	soup = BeautifulSoup( req.content , "html.parser" );
+	data = soup.find_all( "a" , { "class" : "v-align-middle" } );
+	  
+	for link in data:
+	     if downloaded_plugin == True:
+	          break;
+	     #print("Loop1");
+	     #print(link['href']);
+	     req2 = requests.get( "https://github.com" + link[ 'href' ] + "/releases" );
+	     soup2 = BeautifulSoup( req2.content , "html.parser" );
+	     data2 = soup2.find( "ul" , { "class" : "release-downloads"} );
+	     #print(link['href']);
+	     a = data2.find( 'a' , href = True );
+	     #print(a['href']);
+	     if download_file( "https://github.com" + a[ 'href' ] ) is not None:
+	         print( Fore.GREEN + "\nSuccessfully downloaded " + plugin_name );
+	         break;
+
+def GetFunction( ):
     print( Style.BRIGHT );
     print( Fore.WHITE + "\nInput function name to search in wiki" );
     function_name  = input( );
@@ -45,8 +81,25 @@ while check == True:
 
     except IndexError:
         print( Fore.RED + "No results found check your function name (case sensitive)" );
-    
-    print( Fore.WHITE + "\n\n\nDo you want to search more?(Y/N)" );
+
+
+
+print( Style.NORMAL );
+print( "\n\n\t\t\t\t" + Fore.WHITE + "SAMP HELPER" + Fore.MAGENTA + " PYTHON TOOL " + Fore.GREEN + "BY" + Fore.RED + " SREYAS" );
+
+while check == True:
+    print( Fore.WHITE+ "Select your option\n\
+    					1.Search for a function defintion\n\
+    					2.Get a plugin\n\
+    					3.Quit\n" );
+    option = input( );
+    if option == "1":
+    	GetFunction();
+    elif option == "2":
+    	GetPlugin();
+    else:
+    	exit();    
+    print( Fore.WHITE + "\n\n\nDo you want to do anything  more?(Y/N)" );
     NeedMore = input( );
 
     if NeedMore == "n" or NeedMore == "N":
