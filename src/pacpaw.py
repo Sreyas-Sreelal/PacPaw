912f864e1389e759c70c74e316a7908d03545800
__SyS__ <sreyassreelal@gmail.com>
2017-04-29 13:25:47 +0530

    name change

--- src/pacpaw.py
+++ src/pacpaw.py
@@ -1,6 +1,6 @@
 
 """
-Samp Helper a python based package manager for samp built with the power of web scraping.
+PacPaw a python based package manager for pawn language.
 Developer : Sreyas (__SyS__) : https://github.com/Sreyas-Sreelal
 Contributors / Collabrators : None
 
@@ -17,39 +17,39 @@ import click
 Version = "1.0";
 
 
-@click.group(invoke_without_command=True)
+@click.group( invoke_without_command = True )
 @click.pass_context
 
-def cli(ctx):
+def cli( ctx ):
         if ctx.invoked_subcommand is None:
-                print("PacPaw Build" + Version );
-                print("Usage : pacpaw [OPTIONS] COMMANDS [ARGS]\n");
-                print("Use --help to see all available commands");
+                print( "PacPaw Build" + Version );
+                print( "Usage : pacpaw [OPTIONS] COMMANDS [ARGS]\n" );
+                print( "Use --help to see all available commands" );
                 pass
 
-@click.command(help='Provides sa-mp function\'s definitions')
-@click.option('--name', prompt='Your function name',
-              help='The function to refer.')
+@click.command( help = 'Provides sa-mp function\'s definitions' )
+@click.option( '--name', prompt = 'Your function name',
+              help = 'The function to refer.' )
 
-def refer(name): 
+def refer( name ): 
     GetFunction( name );
 
-@click.command(help='Gets samp repo')
-@click.option('--name', prompt='Script\'s name',
-              help='Name of sa-mp script.')
-def getscript(name):
-        GetRepo(name);
+@click.command( help = 'Gets samp repo' )
+@click.option( '--name','-S', prompt='Script\'s name',
+              help='Name of sa-mp script.' )
+def getscript( name ):
+        GetRepo( name );
 
-@click.command(help='Gets samp snippet')
-@click.option('--name', prompt='Snippet\'s name',
-              help='Name of sa-mp snippet.')
-def getsnippet(name):
-        GetSnippet(name);
+@click.command( help = 'Gets samp snippet' )
+@click.option( '--name','-S', prompt = 'Snippet\'s name',
+              help = 'Name of sa-mp snippet.')
+def getsnippet( name ):
+        GetSnippet( name );
 
-cli.add_command(refer);
-cli.add_command(getscript);
-cli.add_command(getsnippet);
+cli.add_command( refer );
+cli.add_command( getscript );
+cli.add_command( getsnippet );
 
 
 if __name__ == '__main__':
-	cli();
+	cli( );
